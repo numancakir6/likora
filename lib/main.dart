7b11554fdd9a024b22f25e1e862cdc409dd06c70
Numan Cakir <numancakir6@gmail.com>
2026-04-03 19:57:37 +0300

    bug 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io' show Platform;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
@@ -7,11 +8,28 @@ import 'splash_page.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  FlutterError.onError = (details) {
+    debugPrint('FlutterError: ${details.exception}');
+    debugPrint('${details.stack}');
+  };
+
   if (!kIsWeb && Platform.isIOS) {
-    await Firebase.initializeApp();
+    try {
+      debugPrint('Firebase init start');
+      await Firebase.initializeApp().timeout(const Duration(seconds: 6));
+      debugPrint('Firebase init OK');
+    } catch (e, st) {
+      debugPrint('Firebase init FAILED: $e');
+      debugPrint('$st');
+    }
   }
 
-  runApp(const LikoraApp());
+  runZonedGuarded(() {
+    runApp(const LikoraApp());
+  }, (e, st) {
+    debugPrint('runZonedGuarded error: $e');
+    debugPrint('$st');
+  });
 }
 
 class LikoraApp extends StatelessWidget {
